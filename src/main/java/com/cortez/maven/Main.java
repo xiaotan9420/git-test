2a12b1c08aedefc6062a3c95ca64d861c18a5528
cortez <2781082260@qq.com>
2023-04-12 14:50:52 +0800

    ideal first-commit

--- src/main/java/com/cortez/maven/Main.java
+++ src/main/java/com/cortez/maven/Main.java
@@ -4,5 +4,7 @@ package com.cortez.maven;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Hello world!");
+
     }
 }
