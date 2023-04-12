704c2416132f2462a8d5c87bfc38cb1c79901c03
cortez <2781082260@qq.com>
2023-04-12 15:00:03 +0800

    ideal third-commit

--- src/main/java/com/cortez/maven/Main.java
+++ src/main/java/com/cortez/maven/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Hello world!---hot-fix");
+        System.out.println("hello world!---hot-fix2");
     }
 }
