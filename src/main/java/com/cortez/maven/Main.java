a6fac63492dd2e4c24e4dd4b07ea038b47a2893e
cortez <2781082260@qq.com>
2023-04-12 14:56:03 +0800

    ideal second-commit

--- src/main/java/com/cortez/maven/Main.java
+++ src/main/java/com/cortez/maven/Main.java
@@ -4,5 +4,6 @@ package com.cortez.maven;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Hello world!---hot-fix");
     }
 }
