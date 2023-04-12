490d786ef3e86f452ff19e3ed6f10e65f909bbfe
cortez <2781082260@qq.com>
2023-04-12 18:27:04 +0800

    push commit

--- src/main/java/com/cortez/maven/Main.java
+++ src/main/java/com/cortez/maven/Main.java
@@ -6,5 +6,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hello world!---hot-fix");
         System.out.println("hello world!---hot-fix2");
+        System.out.println("push commit");
     }
 }
